0fbeb9f626e1acda5bd9fa73e49599afa4898821
Ayan <ayan.chowdhury2050@gmail.com>
2019-07-25 14:34:13 +0530

    Improved Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     scope :sorted, lambda { order("name ASC") }
     scope :search, lambda {|query| where(["name LIKE ? OR email LIKE?", "%#{query}%".titleize, "%#{query}%" ])}
     
-    validates_presence_of :name
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     before_save :sanitize_name
 
     def sanitize_name
