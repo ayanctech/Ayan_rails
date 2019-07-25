1933d4deb28f1f23b00186164a4f93d3014691fd
Ayan <ayan.chowdhury2050@gmail.com>
2019-07-25 21:28:07 +0530

    Introduced Image attached validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
     has_one_attached :picture
+    validates :title, presence: true
+    validate :correct_image
+
+    private
+    
+    def correct_image
+        if picture.attached?
+          true
+        else
+          errors.add(:picture,"should be attached !")
+        end
+    end
 end
